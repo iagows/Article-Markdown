4a6de23eb5d13faad17e3032c6fb0fe38309e6e5
iagows <el.iagows@gmail.com>
2015-11-27 17:45:05 -0300

    First pull
    What I have done to now. More info at readme.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Article-Markdown
 A markdown text editor for creating articles.
+
+## Current status
+You can write your text.
+
+## Next steps
+* Make a better .css
+* Add buttons (chapter, references ...)
+
+# More
+The *marked* folder is a project that doesn't belongs to me. Inside *marked* folder you'll find the license.
+
---
+++ js.js
@@ -0,0 +1,127 @@
+$(function() {
+	var editor = byId('input-text');
+	editor.onkeyup = updatePreview;
+	//botões
+	$("#link").button({
+		icons: {
+			primary: "ui-icon-link"
+		},
+		text: false
+	});
+	$("#image").button({
+		icons: {
+			primary: "ui-icon-image"
+		},
+		text: false
+	});
+	$("#referencia").button({
+		icons: {
+			primary: "ui-icon-tag"
+		},
+		text: false
+	});
+
+	$("#buttons").buttonset();
+	$("#text-bts").buttonset();
+	$("#list-bts").buttonset().next().buttonset();
+	$("#more-bts").buttonset();
+
+	$("#head-bt").click(function(){
+		var opt = byId("headers");
+		var text = opt.options[opt.selectedIndex].label;
+		addAtLineBegining(text+ " ");
+	});
+	$("#headers").selectmenu();
+
+	$("#negrito").click(function() {
+		addAtSelection("**");
+	});
+
+	$("#italico").click(function() {
+		addAtSelection("*");
+	});
+
+	$("#taxado").click(function() {
+		addAtSelection("~~");
+	});
+
+	$("#linha").click(function() {
+		addAtLineBegining("___\n");
+	});
+
+	$("#lista").click(function() {
+		addAtLineBegining("* ");
+	});
+
+
+	$("#ordenada").click(function() {
+		addAtLineBegining("1. ");
+	});
+
+	$("#citacao").click(function() {
+		addAtLineBegining("> ");
+	});
+
+});
+
+function addAt(string, position)
+{
+	var input = byId("input-text");
+	var content = input.value;
+	var newText = content.substring(0, position)+ string + content.substring(position);
+	input.value = newText;
+	//movendo a posição do cursor
+	if(input.setSelectionRange)
+	{
+		input.focus();
+		input.setSelectionRange(position,position);
+	}
+	updatePreview();
+}
+
+function addAtLineBegining(string)
+{
+	var input = byId("input-text");
+	var content = input.value;
+	var temp = content.substring(0, input.selectionStart);
+	var index = temp.lastIndexOf("\n");
+	addAt(string, index+1);
+}
+
+function add(string)
+{
+	var input = byId("input-text");
+	addAt(string, input.selectionStart);
+}
+
+function addAtSelection(string)
+{
+	var input = byId("input-text");
+	var start = input.selectionStart;
+	var end = input.selectionEnd;
+
+	if('selectionStart' in input)
+	{
+		if(start!=end)
+		{
+			addAt(string, end);
+			addAt(string, start);
+		}
+		else
+		{
+			addAt(string+"your text here"+string, start);
+		}
+	}
+}
+
+function byId(id)
+{
+	return document.getElementById(id);
+}
+
+function updatePreview()
+{
+	var receiverDiv = byId('text-receiver');
+	var editor = byId('input-text');
+	var text = editor.value;
+}
