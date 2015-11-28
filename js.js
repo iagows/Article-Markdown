acd8706d0adb620c5c96b948df967fced01f41da
Iago Alves da Silva <el.iagows@gmail.com>
2015-11-28 19:37:24 -0300

    Image
    cleanup

--- js.js
+++ js.js
@@ -68,7 +68,7 @@ $(function() {
 
 function createImageTable()
 {
-	var img = "\n\n| ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Image\") |\n";
+	var img = "\n\n| ![](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png) |\n";
 	var line1 = "| :---: |\n";
 	var sub = "| **Image num**: texto qualquer. |\n";
 
